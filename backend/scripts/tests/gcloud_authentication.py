04d4eb5bc68ea0e842b9d27a59cea23127f23b08
keanukf <forthmann@gmx.net>
2024-10-24 20:36:32 +0200

    Deleted irrelevant experimenting and testing files

--- backend/scripts/tests/gcloud_authentication.py
+++
@@ -1,8 +0,0 @@
-import os
-from google.cloud import storage
-
-os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = '/Users/keanuprivatbenutzer/gcloud_information/chess-chatbot-6e773e8c4ba2.json'
-
-client = storage.Client()
-buckets = list(client.list_buckets())
-print(buckets)
